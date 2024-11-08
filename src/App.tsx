1409c8682c5440e95c66b2b68ecf72860bfd729f
Rennan Miranda <rennan.jsmiranda@gmail.com>
2024-11-08 18:47:14 -0300

    Funcionalidade dashboards

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/home';
+import Dashboard from './pages/dashboards';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path='/' element={<Home />} />
+        <Route path='dashboards' element={<Dashboard/>} />
       </Routes>
     </Router>
   );
